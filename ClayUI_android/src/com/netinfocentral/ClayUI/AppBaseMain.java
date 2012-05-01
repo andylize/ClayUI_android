d786be564365327c6b891dfe634b83ae63122327
Andrew Lize <alize@netinfocentral.com>
2012-05-01 15:28:40 -0500

    Incremental change

--- ClayUI_android/src/com/netinfocentral/ClayUI/AppBaseMain.java
+++ ClayUI_android/src/com/netinfocentral/ClayUI/AppBaseMain.java
@@ -5,14 +5,22 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
-public class AppBaseMain implements AppBaseMainInterface {
+public class AppBaseMain {
     
     // define instance variables
     private String applicationName = "";
     
+    // define data sources
+    private AppPartDataAdapter appPartDataAdapter;
+    
     // default constructor
     public AppBaseMain(String applicationName) {
     	this.applicationName = applicationName;
+    	
+    	// if this is the first run, build tables
+    	if (this.isFirstRun()) {
+    		
+    	}
     }
     
     // properties
@@ -20,9 +28,12 @@ public class AppBaseMain implements AppBaseMainInterface {
     	return this.applicationName;
     }
     
-    public String getDataTableName(int appPartID) {
+    public String getDataTableName(String appPartName) {
     	return "TableName";
     }
     
+    private boolean isFirstRun() { // checks if this is the first time that the application is run by looking for existence of database file
+    	//TODO: check for ClayUI.db
+    	return true;
+    }    
 }
-;
--- ClayUI_android/src/com/netinfocentral/ClayUI/AppBaseMainInterface.java
+++ ClayUI_android/src/com/netinfocentral/ClayUI/AppBaseMainInterface.java
@@ -2,6 +2,6 @@ package com.netinfocentral.ClayUI;
 
 public interface AppBaseMainInterface {
 	
-	public String getDataTableName(int appPartID);
+	public String getDataTableName(String appPartName);
 	
 }
